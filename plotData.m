50ef03c3f825a8e39166f12cb186660bf08a68bf
seanchok <seanchok@gmail.com>
2021-04-28 18:35:44 +0800

    remove acceleration related calculations to reduce memory use
    while working with around 20000 samples, the octave uses up to 10 gigabytes of cpu memory

--- plotData.m
+++ plotData.m
@@ -1,29 +1,6 @@
-function plotData(time, acceleration, velocity, position)
+function plotData(time, position)
   figure;
-  % acceleration
-  subplots(1) = subplot(3, 1, 1);
-  hold on;
-  plot(time, acceleration(:, 1), 'r');
-  plot(time, acceleration(:, 2), 'g');
-  plot(time, acceleration(:, 3), 'b');
-  title('Acceleration');
-  xlabel('seconds');
-  ylabel('m/s/s');
-  legend('x', 'y', 'z');
-
-  % velocity
-  subplots(2) = subplot(3, 1, 2);
-  hold on;
-  plot(time, velocity(:, 1), 'r');
-  plot(time, velocity(:, 2), 'g');
-  plot(time, velocity(:, 3), 'b');
-  title('Velocity');
-  xlabel('seconds');
-  ylabel('m/s');
-  legend('x', 'y', 'z');
-
   % displacement
-  subplots(3) = subplot(3, 1, 3);
   hold on;
   plot(time, position(:, 1), 'r');
   plot(time, position(:, 2), 'g');
@@ -32,8 +9,6 @@ function plotData(time, acceleration, velocity, position)
   xlabel('seconds');
   ylabel('m');
   legend('x', 'y', 'z');
-
-  linkaxes(subplots, 'x');
 
   % maps
   x = position(:, 1);
--- scriptOrientationApproach.m
+++ scriptOrientationApproach.m
@@ -7,11 +7,10 @@ sessionData = importSession('TrackingSession');
 
 time = sessionData.(sessionData.deviceNames{1}).matrix.time;
 rotMRaw = sessionData.(sessionData.deviceNames{1}).matrix.matrix;
-acceleration = sessionData.(sessionData.deviceNames{1}).linear.vector * 9.81;
 
 frequency = 400; % Hz
-shouldUseConstantVelocity = true;
-assumedConstantVelocity = 1; % m/s
+assumedConstantVelocity = 0.119; % m/s
+shouldCorrectBiasRotM = false;
 
 sampleSize = length(time);
 
@@ -21,24 +20,8 @@ for index = 1 : sampleSize
   rotM(:, :, index) = rotMRaw(:, :, index)';
 end
 
-% filter acceleration noise
-function [updatedVector] = filterNoise(vector, lowerBound, upperBound)
-  updatedVector = vector;
-  % if vector is in between the lower and upper bounds, it is considered as noise
-  if (vector > lowerBound && vector < upperBound)
-    updatedVector = 0;
-  endif
-endfunction
-
-thresholds = [-0.1 -0.1 -1; 0.1 0.1 1]; % noise filter threshold for [x, y, z], lower and upper bounds
-for index = 1: length(acceleration)
-  for vectorIndex = 1: 3
-    [acceleration(index, vectorIndex)] = filterNoise(acceleration(index, vectorIndex), thresholds(1, vectorIndex), thresholds(2, vectorIndex));
-  end
-end
-
 % remove first and last few seconds of data
-timeCutoff = [3 3]; % cut off first few and last few seconds of data
+timeCutoff = [163 40]; % cut off first few and last few seconds of data
 rowIndexCutOff = [0 0];
 
 for rowIndex = 1 : sampleSize
@@ -53,57 +36,28 @@ end
 for rowIndex = 1: rowIndexCutOff(1)
   time(1) = [];
   rotM(:, :, 1) = [];
-  acceleration(1, :) = [];
 end
 
 for rowIndex = rowIndexCutOff(2): sampleSize
   time(end) = [];
   rotM(:, :, end) = [];
-  acceleration(end, :) = [];
 end
 
-sampleSize = length(time);
+sampleSize = length(time)
 for rowIndex = 1 : sampleSize
   time(rowIndex) = time(rowIndex) - timeCutoff(1);
 end
 
-% calculate discrete travel distance from linear acceleration
-if (length(acceleration) < sampleSize)
-  acceleration = [acceleration(:, 1); zeros(sampleSize - length(acceleration), 1)];
-else
-  acceleration = acceleration(1 : sampleSize, 1);
-endif
-velocity = zeros(sampleSize); 
 t = 1 / frequency;
-
-for index = 1 : sampleSize
-  v1 = velocity(index);
-  a = acceleration(index);
-  v2 = v1 + a * t;
-  
-  if (index < sampleSize)
-    velocity(index + 1) = v2;
-  endif
-end
-
-% optionally override velocity as an assumed constant
-if (shouldUseConstantVelocity)
-  velocity = zeros(sampleSize) + assumedConstantVelocity;
-endif
 
 distance = zeros(sampleSize);
 
 for index = 1 : sampleSize
-  v1 = velocity(index);
-  
-  if (index == sampleSize)
-    v2 = 0;
-  else
-    v2 = velocity(index + 1);
-  endif
-  
+  v1 = assumedConstantVelocity;
+  v2 = v1;
+
   d = 0.5 * (v1 + v2) * t;
-  
+
   if (d < 0)
     distance(index) = 0;
   else
@@ -123,15 +77,18 @@ position = zeros(sampleSize, 3);
 for index = 1 : sampleSize
   p1 = position(index, :)';
   d = distance(index);
-  
+
   R = rotM(:, :, index);
-  
-  aggregateRotM = biasCorrectionRotM * R;
-  
+
+  aggregateRotM = R;
+  if (shouldCorrectBiasRotM)
+    aggregateRotM = biasCorrectionRotM * R;
+  endif
+
   travelVector = aggregateRotM * [d 0 0]';
-  
+
   p2 = p1 + travelVector;
-  
+
   if (index < sampleSize)
     position(index + 1, :) = p2';
   endif
@@ -141,6 +98,4 @@ end
 position(:, 2) = - position(:, 2);
 
 % plot data
-accelerationData = horzcat(acceleration, zeros(sampleSize, 2));
-velocityData = horzcat(velocity, zeros(sampleSize, 2));
-plotData(time, accelerationData, velocityData, position);
+plotData(time, position);
